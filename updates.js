65034a96643006b9f31e52483e6b283f4ca76f50
harrismowbray <79328175+harrismowbray@users.noreply.github.com>
2025-05-15 17:05:26 -0700

    Add files via upload

--- updates.js
+++ updates.js
@@ -8,7 +8,10 @@ Updates = "<span class='updates_updates' style='color:orange'>Updates</span>: "
 dt = (a) => `<h3>${a.split("/")[0]} ${["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][+a.split("/")[1]-1]} 202${a.split("/")[2]}</h3>`
 
 log = `<pre>
-${dt("3/4/5")}
+${dt("15/5/5")}
+>Braille Converter: Added a Braille proposal for Louisiana Creole
+
+${dt("3/5/5")}
 >Casino Dealer: Made Ante Bonus optional in three card poker
 
 ${dt("24/4/5")}
